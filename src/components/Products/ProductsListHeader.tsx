6b7aef59691f1dee07621941ca433c7f2c020e16
novcmbro <novcmbro@gmail.com>
2024-07-10 20:55:42 -0300

    fixed products list header children type

--- src/components/Products/ProductsListHeader.tsx
+++ src/components/Products/ProductsListHeader.tsx
@@ -1,4 +1,4 @@
-export const ProductsListHeader = ({ title, children }: { title: string, children?: React.ReactElement }) => {
+export const ProductsListHeader = ({ title, children }: { title: string, children?: React.ReactElement | null }) => {
   return (
     <div className="products-list-header">
       <h2 className="typography-title-md">{title}</h2>
