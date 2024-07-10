579ff8a0bd122042390806ba99cbcf82df7a8d29
novcmbro <novcmbro@gmail.com>
2024-07-10 14:20:02 -0300

    fixed field error message id

--- src/components/Field.tsx
+++ src/components/Field.tsx
@@ -5,19 +5,20 @@ import "../styles/field.css"
 
 export const Field = ({ type, textarea, label, price, ...props }: FieldProps) => {
   const { field, fieldState } = useController(props)
+  const errorMessageId = `${field.name}-error-message`
 
   return (
     <div className={classNames("field-container", { "price": price })}>
       {(!textarea || type || price) ? (
-        <input {...field} id={field.name} type={type || "text"} className="field" placeholder="" autoComplete="off" aria-autocomplete="none" aria-describedby="field-error-message" aria-invalid={fieldState.invalid} />
+        <input {...field} id={field.name} type={type || "text"} className="field" placeholder="" autoComplete="off" aria-autocomplete="none" aria-describedby={errorMessageId} aria-invalid={fieldState.invalid} />
       ) : (
-        <textarea {...field} id={field.name} cols={30} rows={2} className="field" placeholder="" autoComplete="off" aria-autocomplete="none" aria-describedby="field-error-message" aria-invalid={fieldState.invalid}></textarea>
+        <textarea {...field} id={field.name} cols={30} rows={2} className="field" placeholder="" autoComplete="off" aria-autocomplete="none" aria-describedby={errorMessageId} aria-invalid={fieldState.invalid}></textarea>
       )}
       <label htmlFor={field.name} className="field-label">
         {label}
       </label>
       {fieldState.invalid ? (
-        <p id="field-error-message" className="field-error-message" role="alert" aria-live="assertive">
+        <p id={errorMessageId} className="field-error-message" role="alert" aria-live="assertive">
           {fieldState.error?.message}
         </p>
       ) : null}
