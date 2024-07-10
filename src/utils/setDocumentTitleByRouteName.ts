f09c00e11e08abb0fb5f1e64e3e76fe0d49f889b
novcmbro <novcmbro@gmail.com>
2024-07-10 14:03:15 -0300

    fixed document title to change on language change

--- src/utils/setDocumentTitleByRouteName.ts
+++ src/utils/setDocumentTitleByRouteName.ts
@@ -1,47 +1,59 @@
 import i18next from "i18next"
 
 export const setDocumentTitleByRouteName = (pathname: string) => {
-  const initialDocumentTitle = document.title
-  const setTitle = (routeNameTranslationKey: string) => {
-    document.title += ` | ${i18next.t(`routes.${routeNameTranslationKey}`)}`
-  }
+  const initialDocumentTitle = "GeekStore"
+  let routeNameTranslationKey = ""
 
   switch (pathname) {
     case "/":
-      setTitle("home")
+      routeNameTranslationKey = "home"
       break
     
     case "/login":
-      setTitle("login")
+      routeNameTranslationKey = "login"
       break
     
     case "/products":
-      setTitle("admin-menu")
+      routeNameTranslationKey = "admin-menu"
       break
     
     case "/add-product":
-      setTitle("add-product")
+      routeNameTranslationKey = "add-product"
       break
-  
+    
     default:
       break
   }
 
   if (pathname.includes("product/")) {
-    setTitle("product-details")
+    routeNameTranslationKey = "product-details"
   }
 
   if (pathname.includes("see-all")) {
-    setTitle("see-all")
+    routeNameTranslationKey = "see-all"
   }
 
   if (pathname.includes("edit-product")) {
-    setTitle("edit-product")
+    routeNameTranslationKey = "edit-product"
   }
 
-  if (document.title === initialDocumentTitle) {
-    setTitle("not-found")
+  if (!!!routeNameTranslationKey) {
+    routeNameTranslationKey = "not-found"
   }
 
-  return () => { document.title = initialDocumentTitle }
+  document.title += ` | ${i18next.t(`routes.${routeNameTranslationKey}`)}`
+
+  const setDocumentTitleOnTranslationChange = () => {
+    if (document.title !== initialDocumentTitle) {
+      document.title = initialDocumentTitle
+    }
+    setDocumentTitleByRouteName(pathname)
+  }
+
+  i18next.on("languageChanged", setDocumentTitleOnTranslationChange)
+
+  return () => {
+    document.title = initialDocumentTitle
+    i18next.off("languageChanged", () => setDocumentTitleOnTranslationChange)
+  }
 }
